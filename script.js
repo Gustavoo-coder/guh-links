4a8922fc66eaa3161331dd0b36ed928829a78f96
Gustavo Rodrigues <gustavoaft78@outlook.com>
2025-04-18 21:42:35 -0300

    :tada: inital commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function toggleMode(){
+  const html= document.documentElement
+
+ //  if(body.classList.contains('ligth')) {
+//  html.classList.remove('ligth')
+//    }else{
+ //  html.classList.add('light') 
+ // }
+
+ html.classList.toggle('light')
+}
